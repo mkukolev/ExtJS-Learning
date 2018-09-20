9d0f6b055aa2289876410776cf8eac089290986f
mkukolev <michael.kukolev.phone@gmail.com>
2018-09-20 18:53:53 +0300

    add store, add grid

--- To Do List/index.js
+++ To Do List/index.js
@@ -1,58 +1,65 @@
+
 Ext.onReady(function() {
-
-    /**
-     * form
-     * grid
-     * window
-     * panel
-     */
-
-    Ext.create('Ext.window.Window', {
-        title: 'my app',
-        autoShow: true,
-        modal: true,
-        resizable: false,
-        closable: false,
-        maximizable: true,
-        minimizable: true,
-        width: 300,
-        height: 300,
-        items: [
-            {
-                xtype: 'panel',
-                region: 'north',
-                height: 200,
-                html: 'header'
-            },
-            {
-                xtype: 'panel',
-                retion: 'center',
-                html: 'center'
-            }
-        ]
+// Сделал store с полями, сюда в data должны записываться задачи для ту-ду-листа.
+    var store = Ext.create('Ext.data.Store', {
+        fields: ['id', 'header', 'description', 'createDate', 'endDate', 'done'],
+        data: {
+                id: '1', header: 'Developing todo-list', description: 'test', createDate: '123', endDate: '123', done: true
+        },
     });
 
-    
-
-    // Ext.create('Ext.panel.Panel', {
-    //     // renderTo: Ext.getBody(),
-    //     layout: 'border',
-    //     width: window.screen.availWidth,
-    //     height: window.screen.availHeight,
-    //     title: 'my app',
-    //     items: [
-    //         {
-    //             xtype: 'panel',
-    //             region: 'north',
-    //             height: 200,
-    //             html: 'header'
-    //         },
-    //         {
-    //             xtype: 'panel',
-    //             retion: 'center',
-    //             html: 'center'
-    //         }
-    //     ]
-    // });
-
+// Делаю грид с 6ю столбцами. Хочу сделать его расположение по центру.
+    Ext.create('Ext.grid.Panel', {
+        title: 'To Do List',
+        width: 800,
+        height: 600,
+        align: 'center',
+        store: store,
+        columns: 
+        [
+            {
+                dataIndex: 'id',
+                width: 50,
+                align: 'center',
+                text: 'ID'
+            },
+            {
+                dataIndex: 'header',
+                width: 200,
+                text: 'Header'
+            },
+            {   
+                dataIndex: 'description',
+                width: 200,
+                text: 'Description'
+            },
+            {
+                dataIndex: 'createDate',
+                width: 100,
+                align: 'center',
+                text: 'Create date'
+            },
+            {
+                dataIndex: 'endDate',
+                width: 100,
+                align: 'center',
+                text: 'End date'
+            },
+            {
+                // Последний столбец - чекбокс для выполненных задач.
+                xtype: 'checkcolumn',
+                dataIndex: 'done',
+                width: 100,
+                align: 'center',
+                text: 'Done work'
+                // Проверялка чекбокса.
+                // listeners: {
+                //     checkchange: function () {
+                //         alert("test");
+                //     }
+                // } 
+            },
+        ],
+        renderTo: Ext.getBody()
+    })
 });
