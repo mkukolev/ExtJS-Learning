e3c39bf903343e08bec1db92e4cfd2085457342c
mkukolev <michael.kukolev.phone@gmail.com>
2018-09-21 12:52:11 +0300

    add essence class

--- To Do List/index.js
+++ To Do List/index.js
@@ -1,22 +1,41 @@
 
+// Сделал сущность для Задачи.
+//var date = Ext.data.Types.DATE;
+
+Ext.define('Task', {
+    extend: 'Ext.data.Model',
+    fields: [
+        {name: 'id', type: 'int'},
+        {name: 'header', type: 'string'},
+        {name: 'description', type: 'string'},
+        {name: 'createDate', type: 'date'},
+        {name: 'endDate', type: 'date'},
+        {name: 'done', type: 'boolean', defaultValue: false},
+    ],
+    validations: [
+        {type: 'length', field: 'header', max: 100},
+        {type: 'length', field: 'description', max: 255}
+    ]
+});
+
 Ext.onReady(function() {
 // Сделал store с полями, сюда в data должны записываться задачи для ту-ду-листа.
     var store = Ext.create('Ext.data.Store', {
         fields: ['id', 'header', 'description', 'createDate', 'endDate', 'done'],
-        data: {
-                id: '1', header: 'Developing todo-list', description: 'test', createDate: '123', endDate: '123', done: true
-        },
+        data: [
+            {id: '1', header: 'Developing todo-list', description: 'test', createDate: '123', endDate: '123', done: true}
+        ]
     });
 
 // Делаю грид с 6ю столбцами. Хочу сделать его расположение по центру.
     Ext.create('Ext.grid.Panel', {
+        renderTo: Ext.getBody(),
         title: 'To Do List',
         width: 800,
         height: 600,
         align: 'center',
         store: store,
-        columns: 
-        [
+        columns: [
             {
                 dataIndex: 'id',
                 width: 50,
@@ -25,12 +44,12 @@ Ext.onReady(function() {
             },
             {
                 dataIndex: 'header',
-                width: 200,
+                width: 224,
                 text: 'Header'
             },
             {   
                 dataIndex: 'description',
-                width: 200,
+                width: 224,
                 text: 'Description'
             },
             {
@@ -60,6 +79,5 @@ Ext.onReady(function() {
                 // } 
             },
         ],
-        renderTo: Ext.getBody()
     })
 });
